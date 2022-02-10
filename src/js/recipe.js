b07057028321ddf40b0d4c86ac80ac9e207c82e5
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-09 16:31:30 -0800

    add API calls for list and recipes
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,3 +1,25 @@
 export default class Recipe {
-  constructor() {}
+  static async getId(ingredient) {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  static async getRecipe(id) {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
 }
