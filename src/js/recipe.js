3aeca2a9dfb63b919020a96c16cf9ba3627710a8
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-02-10 16:19:50 -0800

    add api call for random recipe
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -3,6 +3,7 @@ export default class Recipe {
     try {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
       if (!response.ok) {
+        console.log("apierror");
         throw Error(response.statusText);
       }
       return response.json();
@@ -14,6 +15,18 @@ export default class Recipe {
   static async getRecipe(id) {
     try {
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
+    }
+  }
+
+  static async getRandom() {
+    try {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
